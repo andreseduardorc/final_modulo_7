d7b67b2b30383163b0681866ef273a0bc04b1fbb
andres rodriguez <andreseduardorc@gmail.com>
2023-08-11 16:41:14 -0400

    add function as user.controller

--- App/controllers/user.controller.js
+++ App/controllers/user.controller.js
@@ -1,21 +1,99 @@
-const {users} = require('../models')
-const db = require('../models')
-const User = db.users
-const Bootcamp = db.bootcamps
+const { users } = require("../models");
+const db = require("../models");
+const User = db.users;
+const Bootcamp = db.bootcamps;
 
-exports.createUser = (user)=>{
-    return user.create({
-        firstName : user.firstName,
-        lastName : user.lastName,
-        email: user.email
+exports.createUser = (user) => {
+  return User.create({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  })
+    .then((user) => {
+      console.log(`usuario creado: ${JSON.stringify(user, null, 4)}`);
+      return user;
+    })
+    .catch((err) => {
+      console.log(`error al crear usuario-${err}`);
+    });
+};
 
+exports.findUserById = (userId) => {
+  return User.findByPk(userId, {
+    include: [
+      {
+        model: Bootcamp,
+        as: "bootcamps",
+        attributes: ["id", "title"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  })
+    .then((users) => {
+      return users;
     })
-    .then(user =>{
-        console.log(`usuario creado: ${JSON.stringify(user, null, 4)}`)
-        return user
+    .catch((err) => {
+      console.log(`error al buscar usuarios ${err}`);
+    });
+};
+exports.findAll = () => {
+  return User.findAll({
+    include: [
+      {
+        model: Bootcamp,
+        as: "bootcamps",
+        attributes: ["id", "tittle"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  })
+    .then((users) => {
+      return users;
     })
-    .catch((err)=>{
-        console.log(`error al crear usuario-${err}`)
-    })
+    .catch((err) => {
+      console.log(`error al buscar ${err}`);
+    });
+};
 
-}
+exports.updateUserById = (userId, fName, Lname) => {
+  return User.update(
+    {
+      firstName: fName,
+      lastName: Lname,
+    },
+    {
+      where: {
+        id: userId,
+      },
+    }
+  )
+    .then((user) => {
+      console.log(
+        `se ha actualizado el usuario :${JSON.stringify(user, null, 4)}`
+      );
+      return user;
+    })
+    .catch((err) => {
+      console.log(`error al actualizar usuario ${err}`);
+    });
+};
+
+exports.deleteUserId = (userId) => {
+  return User.destroy({
+    where: {
+      id: userId,
+    },
+  })
+    .then((user) => {
+      console.log(
+        `se ha eliminado el usuario ${JSON.stringify(user, null, 4)}`
+      );
+    })
+    .catch((err) => {
+      console.log(`no se ha podido eliminar el usuario ${err}`);
+    });
+};
